cc6e3bf32037943642b0547085ae001001b51f8f
Davi Sperandio <davisperandio.sp@gmail.com>
2024-10-31 20:16:24 -0300

    Finalizei o 'Put' e 'Delete' de Curriculo

--- src/main/java/com/syndesi/apolloApi/controller/CurriculoController.java
+++ src/main/java/com/syndesi/apolloApi/controller/CurriculoController.java
@@ -5,11 +5,13 @@ import org.springframework.web.bind.annotation.RestController;
 import com.syndesi.apolloApi.model.Curriculo;
 import com.syndesi.apolloApi.service.CurriculoService;
 
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +19,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
 
 
@@ -37,16 +40,22 @@ public class CurriculoController {
         return novoCurriculo;
     }
 
-    // ESTAVA REVISANDO ESSE MÉTODO DAQUI
     @PutMapping("/{id}")
     public ResponseEntity<?> atualizarCurriculo(@PathVariable Long id, @RequestBody Curriculo curriculoAtualizado) {
         try{
             Curriculo curriculo = curriculoService.atualizar(id, curriculoAtualizado);
             return ResponseEntity.ok(curriculo);
         }
-        catch()
-        
-        return entity;
+        catch(EntityNotFoundException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Currículo não encontrado");
+        }
+        catch(Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao atualizar o currículo");
+        }
     }
-    
+
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable Long id){
+        curriculoService.deletar(id);
+    } 
 }
--- src/main/java/com/syndesi/apolloApi/service/CurriculoService.java
+++ src/main/java/com/syndesi/apolloApi/service/CurriculoService.java
@@ -3,6 +3,7 @@ package com.syndesi.apolloApi.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.syndesi.apolloApi.model.Curriculo;
 import com.syndesi.apolloApi.model.Jovem;
@@ -11,6 +12,7 @@ import com.syndesi.apolloApi.repository.CurriculoRepository;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 
+@Service
 public class CurriculoService {
     @Autowired
     private CurriculoRepository curriculoRepository;
@@ -30,5 +32,14 @@ public class CurriculoService {
         Curriculo curriculoExistente = curriculoRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Currículo não encontrado"));
         
         curriculoExistente.setNivelEscolaridade(curriculoAtualizado.getNivelEscolaridade());
+        curriculoExistente.setHabilidadesProfissionais(curriculoAtualizado.getHabilidadesProfissionais());
+        curriculoExistente.setHabilidadesInterpessoais(curriculoAtualizado.getHabilidadesInterpessoais());
+        curriculoExistente.setInformacoesAdicionais(curriculoAtualizado.getInformacoesAdicionais());
+        curriculoExistente.setCertificados(curriculoAtualizado.getCertificados());
+        return curriculoExistente;
+    }
+
+    public void deletar(Long id){
+        curriculoRepository.deleteById(id);
     }
 }
