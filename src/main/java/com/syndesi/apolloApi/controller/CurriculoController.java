566b1ea85868dd927a6240aa98444bfb36c29684
Davi Sperandio <davisperandio.sp@gmail.com>
2024-11-21 16:52:49 -0300

    Adicionei os CrossOrigin

--- src/main/java/com/syndesi/apolloApi/controller/CurriculoController.java
+++ src/main/java/com/syndesi/apolloApi/controller/CurriculoController.java
@@ -21,11 +21,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 
 
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/curriculos")
 public class CurriculoController {
     @Autowired
